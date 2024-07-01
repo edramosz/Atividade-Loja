46bdf5e626244777a0073db0bb656ec42f735ae8
edramosz <163909955+edramosz@users.noreply.github.com>
2024-07-01 16:18:28 -0300

    Initial commit

---
+++ Atividade Loja/Funcionario.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Loja
+{
+    public class Funcionario
+    {
+        public void IniciarMenuFuncionario()
+        {
+            //erro aqui abaixo por conta da falta de ação dentro do menu funcionário.
+            int acaoSelecionada = -1;
+            while (acaoSelecionada != 0)
+            {
+                ExibirMenuFuncionario();
+                acaoSelecionada = SolicitarAcaoUsuario();
+                RealizarAcaoSelecionada(acaoSelecionada);
+            }
+        }
+        public void ExibirMenuFuncionario()
+        {
+            Console.WriteLine("Você deseja fazer qual das seguintes opções?" + "\n0 - Fechar o Sistema." + "\n1 - Verificar Salário." + "\n2 - Carga Horária." + "\n3 - Histórico de Vendas." + "");
+        }
+        private int SolicitarAcaoUsuario()
+        {
+            int acao = -1;
+            while (acao < 0 || acao > 3)
+            {
+                Console.WriteLine("Digite a ação que você deseja realizar.");
+                acao = int.Parse(Console.ReadLine());
+
+                if (acao > 3 || acao < 0)
+                {
+                    Console.WriteLine("Número inválido!!\nFavor digite um número válido.");
+                }
+            }
+            return acao;
+        }
+        private void RealizarAcaoSelecionada(int acaoSelecionada)
+        {
+            //switch (acaoSelecionada)
+            //{
+            //    case 1:
+            //        Gerenciador.ListarFuncionarios();
+            //        Console.WriteLine("\n\n");
+            //        break;
+
+            //    case 2:
+            //        CalcularBonificacao();
+            //        Console.WriteLine("\n\n");
+            //        break;
+
+            //    case 3:
+            //        AdicionarFuncionario();
+            //        Console.WriteLine("\n\n");
+            //        break;
+            //}
+        }
+    }
+}
---
+++ Atividade Loja/Sistema.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Loja
+{
+    public class Sistema
+    {
+        private Funcionario Funcionarios { get; set; }
+        public void IniciarSistema()
+        {
+            int acaoSelecionada = -1;
+            while (acaoSelecionada != 0)
+            {
+                ExibirMenu();
+                acaoSelecionada = SolicitarAcaoUsuario();
+                RealizarAcaoSelecionada(acaoSelecionada);
+            }
+        }
+        private void ExibirMenu()
+        {
+            Console.WriteLine("SEJA MUITO BEM-VINDO AO NOSSO PROGRAMA DA LOJA THEDS." + "\n\nEscolha a ação a ser feita." + "\n0 - Fechar o sistema." + "\n1 - Entrar como Cliente." + "\n2 - Entrar como Funcionário." + "");
+        }
+        private int SolicitarAcaoUsuario()
+        {
+            int acao = -1;
+            while (acao < 0 || acao > 2)
+            {
+                Console.WriteLine("Digite a ação que você deseja realizar.");
+                acao = int.Parse(Console.ReadLine());
+
+                if (acao > 2 || acao < 0)
+                {
+                    Console.WriteLine("Número inválido!!\nFavor digite um número válido.");
+                }
+            }
+            return acao;
+        }
+        private void RealizarAcaoSelecionada(int acaoSelecionada)
+        {
+            switch (acaoSelecionada)
+            {
+                //    case 1:
+                //        Gerenciador.ListarFuncionarios();
+                //        Console.WriteLine("\n\n");
+                //        break;
+
+                case 2:
+                    Funcionarios.IniciarMenuFuncionario();
+                    //Console.WriteLine("\n\n");
+                    break;
+            }
+        }
+
+
+
+
+
+    }
+}
